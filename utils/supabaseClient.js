60c63f6883a01ae45d6e2bb02c1a89d1b32b2307
ImtiazAyesha <ayeshaimtiaz5112003@gmail.com>
2026-03-25 10:41:18 +0500

    Key added

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://vqmengmrbcuqgihbxouy.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_TfCvrA-QalIeDKpkVaeuOg_r6IvpWlc';
-
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxbWVuZ21yYmN1cWdpaGJ4b3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTM4MzMsImV4cCI6MjA4OTQyOTgzM30.CiQyGnzpERrLarmjIYuw9VL6xWN6YLu3Tcs33czMAks';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
